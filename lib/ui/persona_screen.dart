493ef2cd38d208b84fd71ab726198acb1bf54283
Cárol <e_mo04@outlook.com>
2020-10-10 18:03:24 -0500

    Screen con mi commit

--- lib/ui/persona_screen.dart
+++ lib/ui/persona_screen.dart
@@ -29,7 +29,110 @@ class _PersonaScreenState extends State<PersonaScreen> {
   }
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text('Persona'),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        height: 570.0,
+        padding:  const EdgeInsets.all(20.0),
+        child: Card(
+          child: Center(
+            child: Column(
+              children: <Widget> [
+                TextFormField(
+                  controller: _nombreController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 17.0, color: Colors.green),
+                  decoration: InputDecoration(icon: Icon(Icons.accessible_forward_sharp),
+                      labelText: 'Nombre'
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 8.0),),
+                Divider(),
+                TextField(
+                  controller: _ap_patController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 17.0, color: Colors.green),
+                  decoration: InputDecoration(icon: Icon(Icons.accessible_forward_sharp),
+                      labelText: 'Apellido paterno'
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 8.0),),
+                Divider(),
+                TextField(
+                  controller: _ap_matController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 17.0, color: Colors.green),
+                  decoration: InputDecoration(icon: Icon(Icons.accessible_forward_sharp),
+                      labelText: 'Apellido materno'
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 8.0),),
+                Divider(),
+                TextField(
+                  controller: _numController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(fontSize: 17.0, color: Colors.green),
+                  decoration: InputDecoration(icon: Icon(Icons.add_call),
+                      labelText: 'Número'
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 8.0),),
+                Divider(),
+                TextField(
+                  controller: _correoController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 17.0, color: Colors.green),
+                  decoration: InputDecoration(icon: Icon(Icons.email),
+                      labelText: 'Correo'
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 8.0),),
+                Divider(),
+                FlatButton(onPressed:() {
+                  String nombre=_nombreController.text;
+                  String materno=_ap_matController.text;
+                  String paterno=_ap_patController.text;
+                  String num=_numController.text;
+                  String correo=_correoController.text;
+                  if(widget.persona.id !=null){
+                    personasReference.child(widget.persona.id).set({
+                      'nombre' : nombre,
+                      'ap_pat' : paterno,
+                      'ap_mat' : materno,
+                      'num' : num,
+                      'correo' : correo,
+                    }).then((_){
 
+                      Navigator.pop(context);
+                    });
+                  }else {
+                    personasReference.push().set({
+                      'nombre' : nombre,
+                      'ap_pat' : paterno,
+                      'ap_mat' : materno,
+                      'num' : num,
+                      'correo' : correo,
+                    }).then((_){
+                      Navigator.pop(context);
+                    });
+                  }
+                },
+                    child: (widget.persona.id !=null) ? Text('Actualizar'):Text('Agregar')),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
 
