923ec5d111fbec6eb85440709092e4a5a3cc07db
Luis <luis_at3nc0@hotmail.com>
2020-10-12 18:28:39 -0500

    municipio.dart

--- lib/model/persona.dart
+++ lib/model/persona.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-class Persona{
+class Persona {
   String _id;
   String _nombre;
   String _ap_pat;
@@ -9,25 +9,30 @@ class Persona{
   String _num;
   String _correo;
 
+  Persona(this._id, this._nombre, this._ap_pat, this._ap_mat, this._num,
+      this._correo);
 
-  Persona(
-      this._id, this._nombre, this._ap_pat, this._ap_mat, this._num, this._correo);
-
-  Persona.map(dynamic obj){
-    this._nombre= obj['nombre'];
-    this._ap_pat= obj['ap_pat'];
-    this._ap_mat= obj['ap_mat'];
-    this._num= obj['num'];
-    this._correo= obj['correo'];
+  Persona.map(dynamic obj) {
+    this._nombre = obj['nombre'];
+    this._ap_pat = obj['ap_pat'];
+    this._ap_mat = obj['ap_mat'];
+    this._num = obj['num'];
+    this._correo = obj['correo'];
   }
+
   String get id => _id;
+
   String get nombre => _nombre;
+
   String get correo => _correo;
+
   String get num => _num;
+
   String get ap_mat => _ap_mat;
+
   String get ap_pat => _ap_pat;
 
-  Persona.fromSnapShop(DataSnapshot snapshot){
+  Persona.fromSnapShop(DataSnapshot snapshot) {
     _id = snapshot.key;
     _nombre = snapshot.value['nombre'];
     _ap_pat = snapshot.value['ap_pat'];
