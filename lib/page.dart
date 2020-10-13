dbb2f2a5244a4027704336f3b5c662933dcc5ff3
Cárol <e_mo04@outlook.com>
2020-10-12 20:33:37 -0500

    page modificado

--- lib/page.dart
+++ lib/page.dart
@@ -4,86 +4,80 @@ import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'dart:async';
 import 'package:crud_flutter/model/municipio.dart';
-
-class ListViewMunicipios2 extends StatefulWidget{
-  @override
-  _ListViewMunicipiosState createState() => _ListViewMunicipiosState();
-}
-
+ class ListViewMunicipios2 extends StatefulWidget {
+   @override
+   _ListViewMunicipios2State createState() => _ListViewMunicipios2State();
+ }
 final municipiosReference = FirebaseDatabase.instance.reference().child('municipios');
-
-class _ListViewMunicipiosState extends State<ListViewMunicipios2>{
-  List<Municipio> items;
-  StreamSubscription<Event> _onMunicipioAddedSubscripcion;
-  StreamSubscription<Event> _onMunicipioCambioSubscripcion;
-
-
-  @override
+ class _ListViewMunicipios2State extends State<ListViewMunicipios2> {
+   List<Municipio> items;
+   StreamSubscription<Event> _onMunicipioAddedSubscripcion;
+   StreamSubscription<Event> _onMunicipioCambioSubscripcion;
+   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
     items = new List();
   }
-
-
   @override
   void dispose() {
+    // TODO: implement dispose
     super.dispose();
-    _onMunicipioAddedSubscripcion.cancel();
-    _onMunicipioCambioSubscripcion.cancel();
+   // _onMunicipioAddedSubscripcion;
+    //_onMunicipioCambioSubscripcion;
   }
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       title: 'Municipios',
+       home: Scaffold(
+         appBar: AppBar(
+           title: Text('Información'),
+           centerTitle: true,
+           backgroundColor: Colors.lightBlue,
+         ),
+         body: Center(
+           child: ListView.builder(itemCount: items.length,
+               padding: EdgeInsets.only(top: 12.0),
+               itemBuilder: (context, position){
+                 return Column(
+                   children: <Widget>[
+                     Divider(height: 7.0,),
+                     Row(children: <Widget>[
+                       Expanded(child: ListTile(title: Text('${items[position].nombre}',
+                         style: TextStyle(color: Colors.blueAccent, fontSize: 21.0),
+                       ),
+                         subtitle: Text('${items[position].clave}',
+                           style: TextStyle(
+                               color: Colors.blueAccent,
+                               fontSize: 21.0
+                           ),
+                         ),
+                         leading: Column(
+                           children: <Widget>[
+                             CircleAvatar(
+                               backgroundColor: Colors.amberAccent,
+                               radius: 17.0,
+                               child: Text('${items[position].significado}',
+                                 style: TextStyle(
+                                     color: Colors.blueAccent,
+                                     fontSize: 21.0
+                                 ),
+                               ),
+                             )
+                           ],
+                         ),
+                       )),
+                     ],
+                     ),
+                   ],
+                 );
+               }
+           ),
+         ),
+       ),
+     );
+   }
 
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      title: 'Municipios',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Información'),
-          centerTitle: true,
-          backgroundColor: Colors.lightBlue,
-        ),
-        body: Center(
-          child: ListView.builder(itemCount: items.length,
-              padding: EdgeInsets.only(top: 12.0),
-              itemBuilder: (context, position){
-                return Column(
-                  children: <Widget>[
-                    Divider(height: 7.0,),
-                    Row(children: <Widget>[
-                      Expanded(child: ListTile(title: Text('${items[position].nombre}',
-                        style: TextStyle(color: Colors.blueAccent, fontSize: 21.0),
-                      ),
-                          subtitle: Text('${items[position].clave}',
-                            style: TextStyle(
-                                color: Colors.blueAccent,
-                                fontSize: 21.0
-                            ),
-                          ),
-                          leading: Column(
-                            children: <Widget>[
-                              CircleAvatar(
-                                backgroundColor: Colors.amberAccent,
-                                radius: 17.0,
-                                child: Text('${items[position].significado}',
-                                  style: TextStyle(
-                                      color: Colors.blueAccent,
-                                      fontSize: 21.0
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                      )),
-                    ],
-                    ),
-                  ],
-                );
-              }
-          ),
-        ),
-      ),
-    );
-  }
-
-}
+ }
+ 
